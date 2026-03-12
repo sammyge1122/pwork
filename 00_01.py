10f518d074e24133352edcaab343f13ba7ad58ca
sammy <204999212@qq.com>
2026-03-12 16:51:43 +0800

    1

---
+++ 00_01.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    a=10
+    b=1.1
+    c="hello"
+    d=True
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
---
+++ 01_02.py
@@ -0,0 +1,9 @@
+import math
+import cmath
+x=12
+y=1e-5
+print(1+x/math.factorial(3)-y/math.factorial(5))
+print(2*math.log(abs(x-y))/(math.e**(x+y)-math.tan(y)))
+print((math.sin(x)+math.cos(y))/(x**2+y**2)+x**y/(x*y))
+print(math.e**(math.pi/2*x)+math.log10(abs(x-y))/(x+y))
+print(2/(1-cmath.sqrt(7J)))
